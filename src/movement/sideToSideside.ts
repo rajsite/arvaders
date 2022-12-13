8e744da7a7fda1295fdd2da8702ec50f62e67294
rajsite <rajsite@users.noreply.github.com>
2022-12-12 23:15:14 -0600

    Clean-up movement definition

--- src/movement/sideToSideside.ts
+++ src/movement/sideToSideside.ts
@@ -1,7 +1,8 @@
 import { Entity } from "../components/entity";
 import * as w4 from "../wasm4";
+import { Movement } from "./movement";
 
-export function sideToSideMovement (entity: Entity): void {
+export const sideToSideMovement: Movement = (entity: Entity): void => {
     if (entity.position!.vx > 0) {
         entity.position!.x = i32(Math.min(
             entity.position!.x + entity.position!.vx,
